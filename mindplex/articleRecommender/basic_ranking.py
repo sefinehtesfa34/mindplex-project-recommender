25a8c927d1e3f408d983c1f590adc7e203cad72b
Sefineh Tesfa <sefinehtesfa34@gmail.com>
2022-06-30 23:33:01 +0300

    evalute the model

--- mindplex/articleRecommender/basic_ranking.py
+++ mindplex/articleRecommender/basic_ranking.py
@@ -92,7 +92,7 @@ model.compile(optimizer=tf.keras.optimizers.Adagrad(learning_rate=0.1))
 cached_train = train.shuffle(10).batch(5).cache()
 cached_test = test.batch(5).cache()
 model.fit(cached_train, epochs=3)
-
+model.evaluate(cached_test, return_dict=True)
 
 
 
